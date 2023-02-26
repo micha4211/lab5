877cf1dce98aa0d59dbeced1352b7b58073ab3c6
micha4211 <micha4211@icloud.com>
2023-02-25 19:57:59 -0600

    Added UI inputs
    User can specify start and limit

--- src/App.js
+++ src/App.js
@@ -33,6 +33,16 @@ function App() {
   return (
     <div className="App">
     <h1>Lab 6</h1>
+    <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
